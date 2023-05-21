b037a7bd42a567ca60eff6e5c2bb72dc14d15595
elihsane <79572093+elihsane@users.noreply.github.com>
2023-05-21 13:45:14 +0000

    kali script

--- Scripts/kali_setup.sh
+++ Scripts/kali_setup.sh
@@ -8,3 +8,12 @@
 ###############################################
 ###############################################
 
+echo "auto eth1" | sudo tee -a /etc/network/interfaces
+echo "iface eth1 inet static" | sudo tee -a /etc/network/interfaces
+echo "address 192.168.1.61" | sudo tee -a /etc/network/interfaces
+echo "netmask 255.255.255.0" | sudo tee -a /etc/network/interfaces
+
+sudo ifup eth1
+sudo systemctl restart NetworkManager
+
+echo "Script completed"
