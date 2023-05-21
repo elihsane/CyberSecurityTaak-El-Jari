e84bc80af49196e82a2db86234d475dd9978db91
elihsane <79572093+elihsane@users.noreply.github.com>
2023-05-21 14:17:47 +0000

    startup

---
+++ Documentation/Startupplan.md
@@ -0,0 +1,24 @@
+# Startupplan Taak
+
+Dit zijn de stappen die we volgen voor de cybersecurity taak.
+We exploiten de CVE-2021-41773
+
+Student 1: El Ihsane
+Student 2: Jari Roseleth
+
+## Virtuele Machines opzetten.
+
+### Voorzorgen
+
+#### Files
+
+In ons script zitten de VDI files in een folder op onze \C:\ .
+
+* Debian 11 VDI: `C:\VDI Files\Debian 11 (64bit).vdi`
+* KaliLinux VDI: `C:\VDI Files\Kali Linux 2022.3 (64bit).vdi`
+
+#### VDI's
+
+* Download de Debian 11 Desktop (64bit) van https://www.osboxes.org/debian/
+* Download de Kali Linux 2022.3 (All Tools) (64bit) van https://www.osboxes.org/kali-linux/
+
--- VirtueleMachines/Debian11.ps1
+++ VirtueleMachines/Debian11.ps1
@@ -15,7 +15,7 @@ VBoxManage createvm --name $VM_NAME --ostype Debian_64 --register
 # Create the Disk (osboxes)
 VBoxManage createmedium disk --filename "C:\Users\${env:USERNAME}\VirtualBox VMs\${VM_NAME}\${VM_NAME}" --size 5120 --format VDI
 VBoxManage storagectl ${VM_NAME} --name "SATA Controller" --add sata --controller IntelAHCI
-VBoxManage storageattach ${VM_NAME} --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "C:\VDI Files\Debian11(64bit).vdi"
+VBoxManage storageattach ${VM_NAME} --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "C:\VDI Files\Debian 11 (64bit).vdi"
 
 VBoxManage modifyvm $VM_NAME --ioapic on
 VBoxManage modifyvm $VM_NAME --memory 2048 --vram 12
