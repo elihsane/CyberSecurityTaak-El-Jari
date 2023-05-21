3af0307b5c67c78d6c4e98042952be21c6df6540
elihsane <79572093+elihsane@users.noreply.github.com>
2023-05-21 12:57:30 +0000

    powershells

--- VirtueleMachines/Debian11.ps1
+++ VirtueleMachines/Debian11.ps1
@@ -5,3 +5,25 @@
 ##########   debian automatisation   ##########
 ###############################################
 ###############################################
+
+# Name VM
+$VM_NAME = "DebianTaak"
+
+# Create the VM
+VBoxManage createvm --name $VM_NAME --ostype Debian_64 --register
+
+# Create the Disk (osboxes)
+VBoxManage createmedium disk --filename "C:\Users\${env:USERNAME}\VirtualBox VMs\${VM_NAME}\${VM_NAME}" --size 5120 --format VDI
+VBoxManage storagectl ${VM_NAME} --name "SATA Controller" --add sata --controller IntelAHCI
+VBoxManage storageattach ${VM_NAME} --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "C:\VDI Files\Debian11(64bit).vdi"
+
+VBoxManage modifyvm $VM_NAME --ioapic on
+VBoxManage modifyvm $VM_NAME --memory 2048 --vram 12
+VBoxManage modifyvm $VM_NAME --cpus 2
+
+# Network adapters
+VBoxManage modifyvm $VM_NAME --nic1 nat
+VBoxManage modifyvm $VM_NAME --nic2 intnet
+
+# start vm (met gui)
+VBoxManage startvm $VM_NAME --type gui
--- VirtueleMachines/KaliLinux.ps1
+++ VirtueleMachines/KaliLinux.ps1
@@ -5,3 +5,25 @@
 ##########    Kali automatisation    ##########
 ###############################################
 ###############################################
+
+# Name VM
+$VM_NAME = "KaliTaak"
+
+# Create the VM
+VBoxManage createvm --name $VM_NAME --ostype KaliLinux --register
+
+# Create the Disk (osboxes)
+VBoxManage createmedium disk --filename "C:\Users\${env:USERNAME}\VirtualBox VMs\${VM_NAME}\${VM_NAME}" --size 5120 --format VDI
+VBoxManage storagectl ${VM_NAME} --name "SATA Controller" --add sata --controller IntelAHCI
+VBoxManage storageattach ${VM_NAME} --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "C:\VDI Files\Kali Linux 2022.3 (64bit).vdi"
+
+VBoxManage modifyvm $VM_NAME --ioapic on
+VBoxManage modifyvm $VM_NAME --memory 2048 --vram 12
+VBoxManage modifyvm $VM_NAME --cpus 2
+
+# Network adapters
+VBoxManage modifyvm $VM_NAME --nic1 nat
+VBoxManage modifyvm $VM_NAME --nic2 intnet
+
+# start vm (met gui)
+VBoxManage startvm $VM_NAME --type gui
