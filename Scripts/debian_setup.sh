2d52b5dfadfd5d341fabcafbb29505c24493bf37
elihsane <79572093+elihsane@users.noreply.github.com>
2023-05-21 16:06:10 +0000

    debug

--- Scripts/debian_setup.sh
+++ Scripts/debian_setup.sh
@@ -14,7 +14,7 @@ sudo apt-get install apt-transport-https ca-certificates curl gnupg -y
 #install docker
 sudo wget https://download.docker.com/linux/debian/gpg
 sudo apt-key add gpg
-echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee -a /etc/apt/soruces.list.d/docker.list 
+echo 'deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable' | sudo tee -a /etc/apt/soruces.list.d/docker.list 
 
 sudo apt update -y
 
@@ -27,7 +27,7 @@ sudo docker pull httpd:2.4.49
 sudo docker run --name vuln-httpd -p 8080:80 -d httpd:2.4.49
 sudo docker cp vuln-httpd:/usr/local/apache2/conf/httpd.conf .
 
-sed "250s/denied/granted/" httpd.conf > httpd.new.conf
+sed '250s/denied/granted/' httpd.conf > httpd.new.conf
 sudo docker cp httpd.new.conf vuln-httpd:/usr/local/apache2/conf/httpd.conf
 sudo docker container restart vuln-httpd
 
@@ -37,6 +37,6 @@ sudo docker container restart vuln-httpd
 #config for RCE
 sudo docker cp vuln-httpd:/usr/local/apache2/conf/httpd.conf .
 
-sed "184,187s/#//" httpd.conf > httpd.new.conf
+sed '184,187s/#//' httpd.conf > httpd.new.conf
 sudo docker cp httpd.new.conf vuln-httpd:/usr/local/apache2/conf/httpd.conf
 sudo docker container restart vuln-httpd
